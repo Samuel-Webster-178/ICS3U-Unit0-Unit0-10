36e7f5c4f2f726abffa4968a1f7dca0a3c9c707e
Samuel-Webster-178 <samuel.webster@mths.ca>
2022-03-11 19:01:10 +0000

    init

---
+++ rounding_error.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by Samuel Webster
+# Created on March 2022
+# This program adds 0.1 and 0.2
+
+
+def main():
+    # I add 0.1 and 0.2
+
+    # process
+    sum = 0.1 + 0.2
+
+    # output
+    print("0.1 + 0.2 = {}".format(sum))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
